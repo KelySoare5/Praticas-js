010d626b921eb03c8cc51854907136f2254e908e
Kely do Nascimento Soares <111580529+KelySoare5@users.noreply.github.com>
2024-04-16 19:38:26 -0300

    adicionando media-query

--- exercicio001/script.js
+++ exercicio001/script.js
@@ -2,21 +2,22 @@ function carregar(){
     var msg = window.document.getElementById("msg")
     var foto = window.document.getElementById("foto")
     var agora = new Date()
-    var hora = agora.getHours()
-    //var hora = 19 // testando
+    //var hora = agora.getHours()
+    var hora = 1 // testando
     msg.innerHTML = `Agora são ${hora} horas <br>`
 
     if (hora <12){
         msg.innerHTML+="Bom dia!"
         foto.src = 'img/fotomanha.jpg'
         document.body.style.background = "linear-gradient(to bottom, #87CEFA, #FFDAB9)";
+
         
 
     } else if (hora <18){
         msg.innerHTML+="Boa tarde!"
         foto.src = "img/fototarde.jpg"
         document.body.style.background = "linear-gradient(to bottom, #F0E68C, #87CEEB)";
-
+        
 
 
     } else {
