2019347b0ec3d7782cfe91b604d38d627e8c4f6a
Kely do Nascimento Soares <111580529+KelySoare5@users.noreply.github.com>
2024-04-16 18:56:41 -0300

    ajuste de foto e cores

--- exercicio001/script.js
+++ exercicio001/script.js
@@ -3,25 +3,27 @@ function carregar(){
     var foto = window.document.getElementById("foto")
     var agora = new Date()
     var hora = agora.getHours()
-    //var hora = 19
+    //var hora = 19 // testando
     msg.innerHTML = `Agora são ${hora} horas <br>`
 
     if (hora <12){
         msg.innerHTML+="Bom dia!"
         foto.src = 'img/fotomanha.jpg'
-        document.body.style.background="#EBB62F"
+        document.body.style.background = "linear-gradient(to bottom, #87CEFA, #FFDAB9)";
+        
 
     } else if (hora <18){
         msg.innerHTML+="Boa tarde!"
         foto.src = "img/fototarde.jpg"
-        document.body.style.background="#EB7B43"
+        document.body.style.background = "linear-gradient(to bottom, #F0E68C, #87CEEB)";
+
 
 
     } else {
         msg.innerHTML+="Boa noite!"
         foto.src = "img/fotonoite.jpg"
-        document.body.style.background = "linear-gradient(to bottom, #add8e6, #00008b)";
-
+        document.body.style.background = "linear-gradient(to bottom, #000033, #000066)";
+ 
     }
 
 }
