cfcc077f047671015a46c8b67cf4188a96011dda
Kely do Nascimento Soares <111580529+KelySoare5@users.noreply.github.com>
2024-04-22 17:32:55 -0300

    adicionando operadores

--- tabuada/script.js
+++ tabuada/script.js
@@ -1,17 +1,61 @@
 function gerarTabuada(){
     var n = document.getElementById("num")
     var b = document.getElementById("tabuada")
+    var operadores = document.getElementsByName("operadores")
+    //Caixa vazia
+    if (n.value.length == 0){
+        alert("Por favor, digite um numero!")
+        
+    } 
+    // Numero maior que zero
+    else{ 
+        var num = Number(n.value)
+        b.innerHTML = " "
+        // Adição
+        if (operadores[0].checked){ 
 
-    var num = Number(n.value)
-
-    if (num == 0){
-        alert("Numero invalido")
-    } else{
-        var c = 0
-        while(c<=10){
-            var op = document.createElement('option')
-            op.text = `${num} x ${c} = ${num*c}`
-            b.appendChild(op)
+            for (var c = 1; c<=10; c++){
+                var opcao = document.createElement('option')
+                var resultado = num + c
+                opcao.text = `${num} + ${c} = ${resultado}`
+                b.appendChild(opcao)
+            } 
         }
+        //Subtração
+        else if (operadores[1].checked){ 
+            for (var c = 1; c <= 10; c++){
+                var opcao = document.createElement('option')
+                var resultado = num - c
+                opcao.text = `${num} - ${c} = ${resultado}`
+                b.appendChild(opcao)
+            }
+        }
+        //Multiplicação
+        else if (operadores[2].checked){ 
+            for (var c = 1; c <= 10; c++){
+                var opcao = document.createElement('option')
+                var resultado = num * c
+                opcao.text = `${num} * ${c} = ${resultado}`
+                b.appendChild(opcao)
+            }
+        }
+        //Divisão
+        else if (operadores[3].checked){ 
+            for (var c = 1; c <= 10; c++){
+                var opcao = document.createElement('option')
+                var resultado = num / c
+                opcao.text = `${num} / ${c} = ${formatarNumero(resultado)}`
+                b.appendChild(opcao)
+            }
+        }
+    } 
+}
+
+function formatarNumero(num) {
+    var partes = num.toString().split("."); // Divide o número em parte inteira e decimal
+    if (partes.length > 1 && partes[1].length > 2) { // Verifica se a parte decimal existe e se tem mais de 2 dígitos
+        return num.toFixed(2); // Arredonda para duas casas decimais se necessário
+    } else {
+        return num; // Retorna o número como está se não precisar de arredondamento
     }
 }
