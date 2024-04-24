4afa73f321ecba984bde7d6c44d41135a98c89e2
Kely do Nascimento Soares <111580529+KelySoare5@users.noreply.github.com>
2024-04-24 17:55:51 -0300

    aLteração no exer004

--- tabuada/script.js
+++ tabuada/script.js
@@ -2,54 +2,134 @@ function gerarTabuada(){
     var n = document.getElementById("num")
     var b = document.getElementById("tabuada")
     var operadores = document.getElementsByName("operadores")
+    var op_tela_menor = document.getElementById("opera")
+
+
     //Caixa vazia
     if (n.value.length == 0){
         alert("Por favor, digite um numero!")
         
     } 
     // Numero maior que zero
-    else{ 
+    else{
         var num = Number(n.value)
         b.innerHTML = " "
-        // Adição
-        if (operadores[0].checked){ 
+        if (window.matchMedia('(max-width: 992px)').matches) {
+            var op = op_tela_menor.value
+            for (var c = 1; c <= 10; c++){
+                var opcoes_tela_menor = document.createElement('option')
+                switch (op){
+                    case "adc":
+                        var resultado_tela_menor = num + c
+                        opcoes_tela_menor.text =  `${num} + ${c} = ${resultado_tela_menor}`
+                        break
 
-            for (var c = 1; c<=10; c++){
-                var opcao = document.createElement('option')
-                var resultado = num + c
-                opcao.text = `${num} + ${c} = ${resultado}`
-                b.appendChild(opcao)
-            } 
-        }
-        //Subtração
-        else if (operadores[1].checked){ 
-            for (var c = 1; c <= 10; c++){
-                var opcao = document.createElement('option')
-                var resultado = num - c
-                opcao.text = `${num} - ${c} = ${resultado}`
-                b.appendChild(opcao)
+                    case "sub":
+                        var resultado_tela_menor = num - c
+                        opcoes_tela_menor.text =  `${num} - ${c} = ${resultado_tela_menor}`
+                        b.appendChild(opcoes_tela_menor);
+                        break
+
+                    case "mul":
+                        var resultado_tela_menor = num * c
+                        opcoes_tela_menor.text =  `${num} x ${c} = ${resultado_tela_menor}`
+                        break
+
+                    case "divi":
+                        var resultado_tela_menor = num / c
+                        opcoes_tela_menor.text =  `${num} / ${c} = ${formatarNumero(resultado_tela_menor)}`
+                        break
+
+                    default:
+                        opcao.text = "Operação desconhecida";
+                        break;
+                }
+                b.appendChild(opcoes_tela_menor);
+
+            }
+            // Executa o código para telas menores que 992px
+            // alert('A largura da tela é menor que 992px');
+            // Coloque aqui as ações que você quer realizar
+        } else {
+            if (operadores[0].checked){ 
+
+                for (var c = 1; c<=10; c++){
+                    var opcao = document.createElement('option')
+                    var resultado = num + c
+                    opcao.text = `${num} + ${c} = ${resultado}`
+                    b.appendChild(opcao)
+                } 
+            }
+            //Subtração
+            else if (operadores[1].checked){ 
+                for (var c = 1; c <= 10; c++){
+                    var opcao = document.createElement('option')
+                    var resultado = num - c
+                    opcao.text = `${num} - ${c} = ${resultado}`
+                    b.appendChild(opcao)
+                }
+            }
+            //Multiplicação
+            else if (operadores[2].checked){ 
+                for (var c = 1; c <= 10; c++){
+                    var opcao = document.createElement('option')
+                    var resultado = num * c
+                    opcao.text = `${num} * ${c} = ${resultado}`
+                    b.appendChild(opcao)
+                }
+            }
+            //Divisão
+            else if (operadores[3].checked){ 
+                for (var c = 1; c <= 10; c++){
+                    var opcao = document.createElement('option')
+                    var resultado = num / c
+                    opcao.text = `${num} / ${c} = ${formatarNumero(resultado)}`
+                    b.appendChild(opcao)
+                }
             }
         }
-        //Multiplicação
-        else if (operadores[2].checked){ 
-            for (var c = 1; c <= 10; c++){
-                var opcao = document.createElement('option')
-                var resultado = num * c
-                opcao.text = `${num} * ${c} = ${resultado}`
-                b.appendChild(opcao)
-            }
-        }
-        //Divisão
-        else if (operadores[3].checked){ 
-            for (var c = 1; c <= 10; c++){
-                var opcao = document.createElement('option')
-                var resultado = num / c
-                opcao.text = `${num} / ${c} = ${formatarNumero(resultado)}`
-                b.appendChild(opcao)
-            }
-        }
+        // var num = Number(n.value)
+        // b.innerHTML = " "
+        // Adição
+        // if (operadores[0].checked){ 
+
+        //     for (var c = 1; c<=10; c++){
+        //         var opcao = document.createElement('option')
+        //         var resultado = num + c
+        //         opcao.text = `${num} + ${c} = ${resultado}`
+        //         b.appendChild(opcao)
+        //     } 
+        // }
+        // //Subtração
+        // else if (operadores[1].checked){ 
+        //     for (var c = 1; c <= 10; c++){
+        //         var opcao = document.createElement('option')
+        //         var resultado = num - c
+        //         opcao.text = `${num} - ${c} = ${resultado}`
+        //         b.appendChild(opcao)
+        //     }
+        // }
+        // //Multiplicação
+        // else if (operadores[2].checked){ 
+        //     for (var c = 1; c <= 10; c++){
+        //         var opcao = document.createElement('option')
+        //         var resultado = num * c
+        //         opcao.text = `${num} * ${c} = ${resultado}`
+        //         b.appendChild(opcao)
+        //     }
+        // }
+        // //Divisão
+        // else if (operadores[3].checked){ 
+        //     for (var c = 1; c <= 10; c++){
+        //         var opcao = document.createElement('option')
+        //         var resultado = num / c
+        //         opcao.text = `${num} / ${c} = ${formatarNumero(resultado)}`
+        //         b.appendChild(opcao)
+        //     }
+        // }
     } 
 }
+
 
 function formatarNumero(num) {
     var partes = num.toString().split("."); // Divide o número em parte inteira e decimal
