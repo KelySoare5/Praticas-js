f8e5abefd9d7fd9a071b3f2687bf3d2e472cf62f
Kely do Nascimento Soares <111580529+KelySoare5@users.noreply.github.com>
2024-04-24 21:15:03 -0300

    compativel com diferentes telas

--- hora-dia/script.js
+++ hora-dia/script.js
@@ -4,26 +4,24 @@ function carregar(){
     var footer = document.querySelector('footer p');
     var agora = new Date()
     var hora = agora.getHours()
-    //var hora = 19 // testando
+    //var hora = 1 // testando
     msg.innerHTML = `Agora são ${hora} horas <br>`
 
     if (hora <12){
         msg.innerHTML+="Bom dia!"
         foto.src = 'img/fotomanha.jpg'
-        document.body.style.background = "linear-gradient(to bottom, #87CEFA, #FFDAB9)"; //cor de fundo
-        footer.style.color = 'black'; //cor do footer        
+        document.body.style.background = "linear-gradient(to right, #87CEFA, #FFDAB9)"; //cor de fundo       
 
     } else if (hora <18){
         msg.innerHTML+="Boa tarde!"
         foto.src = "img/fototarde.jpg"
-        document.body.style.background = "linear-gradient(to bottom, #F0E68C, #87CEEB)";
+        document.body.style.background = "linear-gradient(to right, #F0E68C, #87CEEB)";
         
-
 
     } else {
         msg.innerHTML+="Boa noite!"
         foto.src = "img/fotonoite.jpg"
-        document.body.style.background = "linear-gradient(to bottom, #000033, #000066)";
+        document.body.style.background = "linear-gradient(to right, #000033, #000066)";
  
     }
 
