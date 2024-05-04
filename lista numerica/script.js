b067df436b1aab0c3e3d31025e52fd13dfd3f84f
Kely do Nascimento Soares <111580529+KelySoare5@users.noreply.github.com>
2024-05-04 16:43:34 -0300

    add soma e media dos valores

--- lista numerica/script.js
+++ lista numerica/script.js
@@ -42,20 +42,35 @@ function finalizar(){
         alert("Caixa vazia! Adicione numeros")
     } else{
         var maior = lista_numerica[0]
-        var menor = lista_numerica[0]        
+        var menor = lista_numerica[0]    
+        var soma = 0    
+        var media = 0
         for (var pos in lista_numerica){
+            soma += lista_numerica[pos]
             if (lista_numerica[pos] > maior)
                 maior = lista_numerica[pos]
             
             if (lista_numerica[pos] < menor)
                 menor = lista_numerica[pos]
-            
         }
+        media = soma / lista_numerica.length
         res.innerHTML= `<p>[${lista_numerica}]</p>`
         res.innerHTML += `<p>Foram registrados ${lista_numerica.length} numeros</p>`
         res.innerHTML += `<p>Maior valor adicionado foi ${maior}</p>`
         res.innerHTML += `<p> O menor valor adicionado foi ${menor}</p>`
+        res.innerHTML += `<p> A soma dos valores é ${soma}`
+        res.innerHTML += `<p> A média dos valores é ${formatarNumero(media)}`
 
         
+    }
+}
+
+
+function formatarNumero(num) {
+    var partes = num.toString().split("."); // Divide o número em parte inteira e decimal
+    if (partes.length > 1 && partes[1].length > 2) { // Verifica se a parte decimal existe e se tem mais de 2 dígitos
+        return num.toFixed(2); // Arredonda para duas casas decimais se necessário
+    } else {
+        return num; // Retorna o número como está se não precisar de arredondamento
     }
 }
